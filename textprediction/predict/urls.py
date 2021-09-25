d1f585c84da6f51c757bc355e25fca8f1671908f
Rachell Calhoun <rachell.calhoun@gmail.com>
2021-09-24 21:57:57 -0400

    update model and vis

--- textprediction/predict/urls.py
+++ textprediction/predict/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('unsupervised/visualization/base_lda/mmds/', TemplateView.as_view(template_name="predict/lda_base_mmds.html"), name="base_mmds"),
     path('unsupervised/visualization/base_lda/pcoa/', TemplateView.as_view(template_name="predict/lda_base_pcoa.html"), name="base_pcoa"),
 
-    path('unsupervised/visualization/bigram_lda/tsne/', TemplateView.as_view(template_name="predict/lda_base_tsne.html"), name="bigram_tsne"),
-    path('unsupervised/visualization/bigram_lda/mmds/', TemplateView.as_view(template_name="predict/lda_base_mmds.html"), name="bigram_mmds"),
-    path('unsupervised/visualization/bigram_lda/pcoa/', TemplateView.as_view(template_name="predict/lda_base_pcoa.html"), name="bigram_pcoa"),
+    path('unsupervised/visualization/bigram_lda/tsne/', TemplateView.as_view(template_name="predict/lda_tsne.html"), name="bigram_tsne"),
+    path('unsupervised/visualization/bigram_lda/mmds/', TemplateView.as_view(template_name="predict/lda_mmds.html"), name="bigram_mmds"),
+    path('unsupervised/visualization/bigram_lda/pcoa/', TemplateView.as_view(template_name="predict/lda_pcoa.html"), name="bigram_pcoa"),
     path('unsupervised/visualization/gsdmm/plotly/', TemplateView.as_view(template_name="predict/plotly_topics.html"), name="topics"),
     path('unsupervised/', TemplateView.as_view(template_name='predict/unsupervised_index.html'), name="unsupervised"),
     path('resources/', TemplateView.as_view(template_name="predict/resources.html"), name='resources'),
--- textprediction/predict/views.py
+++ textprediction/predict/views.py
@@ -22,6 +22,7 @@ def getPredictions(text):
 
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     # my_file = os.path.join(THIS_FOLDER, 'forest_model_small1.sav')
+    # updated model
     my_file = os.path.join(THIS_FOLDER, 'forestmodel1.pk')
     model = joblib.load(open(my_file, "rb"))
     prediction = model.predict(text)
