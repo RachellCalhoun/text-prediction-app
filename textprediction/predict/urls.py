17bbbf160484ae66a9e453ab0d7c73387aa3051f
Rachell Calhoun <rachell.calhoun@gmail.com>
2021-09-26 16:49:54 -0400

    add topic pred

--- textprediction/predict/urls.py
+++ textprediction/predict/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('unsupervised/visualization/bigram_lda/mmds/', TemplateView.as_view(template_name="predict/lda_mmds.html"), name="bigram_mmds"),
     path('unsupervised/visualization/bigram_lda/pcoa/', TemplateView.as_view(template_name="predict/lda_pcoa.html"), name="bigram_pcoa"),
     path('unsupervised/visualization/gsdmm/plotly/', TemplateView.as_view(template_name="predict/plotly_topics.html"), name="topics"),
-    path('unsupervised/', TemplateView.as_view(template_name='predict/unsupervised_index.html'), name="unsupervised"),
+    path('unsupervised/', views.unsupervised_index, name="unsupervised"),
     path('resources/', TemplateView.as_view(template_name="predict/resources.html"), name='resources'),
-
+    path('unsupervised/prediction/results/', views.topic_result, name='topic_prediction'),
 ]
--- textprediction/predict/views.py
+++ textprediction/predict/views.py
@@ -6,6 +6,32 @@ from .forms import TextInputForm
 # import gensim
 import joblib
 import os
+from gensim.parsing import preprocess_string
+import pickle
+from lime.lime_text import LimeTextExplainer
+
+TOPIC_NAMES = [
+    "States, Cities, & Districts (US)",
+    "Sports Teams and Players",
+     "Entertainment (Movies, Games, Films)",
+    "France Locations",
+    "Religion",
+    "Hurricanes and Tropical Storms",
+    "Kings, Leaders, & Presidents Lives",
+    "Music and Composition",
+    "World Teams Championships",
+    "Places, People, Cultures",
+    "Wars and Governments",
+    "Numbers and Cells",
+    "Cities of the World",
+    "Pages and Users",
+    "Football and sports Players",
+    "Education and Work",
+    "Bands & Music Releases",
+    "Known Species & Plants",
+    "Music, Plays, Pieces of Work",
+    "People"
+]
 
 def index(request):
     template = loader.get_template('predict/supervised.html')
@@ -48,6 +74,53 @@ def result(request):
             # topic = get_topic(text)
             template = loader.get_template('predict/result.html')
             return HttpResponse(template.render({'result': result, 'text': text}, request))
+        else:
+            error_message = "Form is not valid please try again"
+    else:
+        error_message = "There was an error, please go back and try again"
+    return HttpResponse(error_message)
+
+
+def unsupervised_index(request):
+    template = loader.get_template('predict/unsupervised_index.html')
+    form = TextInputForm()
+    return HttpResponse(template.render({'form': form}, request))
+
+# our result page view
+def topic_result(request):
+    form = TextInputForm()
+    if request.method == 'POST':
+        form = TextInputForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data.get('text')
+            THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+            # updated model
+            my_file = os.path.join(THIS_FOLDER, 'vec_class_model.pickle')
+            with open(my_file, 'rb') as f:
+                vectorizer, movieT, model = pickle.load(f)
+            predictor = model.predict_proba
+            # Create a LimeTextExplainer
+            explainer = LimeTextExplainer(
+                # Specify split option for string
+                split_expression=lambda x: x.split(),
+                # Our classifer uses N-grams or contextual ordering to classify text
+                # Hence, order matters, and we cannot use bag of words.
+                bow=False,
+                # Specify class names for this case
+                class_names=TOPIC_NAMES
+            )
+
+            # Make a prediction and explain it:
+            exp = explainer.explain_instance(
+                text,
+                classifier_fn=predictor,
+                top_labels=1,
+                num_features=20,
+                num_samples=1000,
+            )
+            exp = exp.as_html()
+            template = loader.get_template('predict/topic_prediction.html')
+            return HttpResponse(template.render({'exp': exp, 'text': text, }, request))
         else:
             error_message = "Form is not valid please try again"
     else:
